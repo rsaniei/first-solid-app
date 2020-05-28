7699728588c938fd07d2a0fab9eb667d8e184727
ranas70 <saniei.r@gmail.com>
2020-05-28 16:59:24 +0200

    Adding functionality based on solid LoggedIn and LoggedOn buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LoggedOut, LoggedIn, LoginButton } from '@solid/react';
+import { Dashboard } from './components/Dashboard';
+// import { PodConnecter } from './components/PodConnecter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// wrap components that should be visible to people who are not
+// logged in inside a < LoggedOut > component, and those that should
+// only be visible to those who are logged in inside a<LoggedIn>.
+const App: React.FC = () => {
+  return <>
+    <React.StrictMode>
+      <LoggedOut>
+        <section className="section">
+          <p className="content">Please connect to your Pod to send a Helo world message!</p>
+          <p className="content">
+            <LoginButton popup="popup.html" className="button is-large is-primary">Connect</LoginButton>
+          </p>
+        </section>
+      </LoggedOut>
+      <LoggedIn>
+        <Dashboard />
+      </LoggedIn>
+    </React.StrictMode>
+  </>;
 }
 
 export default App;
